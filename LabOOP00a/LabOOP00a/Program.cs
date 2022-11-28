3502de94e44fcc36627ceff4f974e690e7edf2fd
Befirst46 <arun.s@kkumail.com>
2022-11-28 16:40:08 +0700

    Student Weight

--- LabOOP00a/LabOOP00a/Program.cs
+++ LabOOP00a/LabOOP00a/Program.cs
@@ -4,11 +4,32 @@ namespace LabOOP00a
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("อรัญ ศรีสวัสดิ์");
-            Console.WriteLine("643450095-5");
-            Console.WriteLine("arun.s@kkumail.com");
+            Student student1 = new Student();
+            student1.name = "นายกฤตวัฒน์ อินทรสิทธิ์";
+            student1.weight = 45.6f;
+
+            Student student2 = new Student();
+            student2.name = "นางสาวณัฐธิดา บุญพา";
+            student2.weight = 65.8f;
+
+            Student student3 = new Student();
+            student3.name = "นายเทพทินกร พรมโสภา";
+            student3.weight = 54.9f;
+
+            Student student4 = new Student();
+            student4.name = "นายรัตพงษ์ ปานเจริญ";
+            student4.weight = 74.2f;
+
+            Student student5 = new Student();
+            student5.name = "นางสาวอฆพร ไร่ขาม";
+            student5.weight = 56.7f;
+
+            float alweight = (student1.weight + student2.weight + student3.weight + student4.weight + student5.weight)/2;
+            Console.WriteLine("All Student wight = "+ alweight);
+
         }
     }
 }
