d6c00dd4f7c2790b31e2b7f3ab3eddfa9ae90585
Befirst46 <arun.s@kkumail.com>
2022-11-28 15:45:40 +0700

    Create Program.cs

---
+++ LabOOP00a/LabOOP00a/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LabOOP00a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("อรัญ ศรีสวัสดิ์");
+            Console.WriteLine("643450095-5");
+            Console.WriteLine("arun.s@kkumail.com");
+        }
+    }
+}
